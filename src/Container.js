69b442472d8287aa612ee7ea0c0511db465d3f25
Katerynapi0 <128479588+Katerynapi0@users.noreply.github.com>
2024-04-17 21:23:24 +0300

    first commit

---
+++ src/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Container.css';
+
+function Container() {
+  return (
+    <div className="container">
+      <h2>Заголовок</h2>
+      <p>Тут могла би бу стаття 🤓</p>
+    </div>
+  );
+}
+
+export default Container;
---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <ul>
+        <li>Меню №1</li>
+        <li>Меню №2</li>
+        <li>Меню №3</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
